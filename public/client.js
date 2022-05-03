e6548bb7b1c65313a5cfe2a03588101dd6b4588f
Samanuay <200303105038@paruluniversity.ac.in>
2022-05-03 12:26:00 +0530

    Web Chat App Project

---
+++ public/client.js
@@ -0,0 +1,42 @@
+var socket = io('http://localhost:8000');
+var audio = new Audio('tone.mp3');
+
+var form = document.getElementById('form');
+var input = document.getElementById('text');
+var container = document.querySelector('.chatbox');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    var message = input.value;
+    append(`You: ${message}`, 'right');
+    if (input.value) {
+        socket.emit('send', message);
+        input.value = '';
+    }
+});
+
+const append = (message, position) => {
+    const msg = document.createElement('div');
+    msg.innerText = message;
+    msg.classList.add('chat');
+    msg.classList.add(position);
+    container.append(msg);
+    if (position == 'left') {
+        audio.play();
+    }
+};
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right');
+});
+
+let n = prompt("Enter your name to join");
+socket.emit('new-user-joined', n);
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+});
+
+socket.on('left', name => {
+    append(`${name} left the chat`, 'right');
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+const users = {};
+
+io.on('connection', socket =>{
+    socket.on('new-user-joined', name=>{
+        console.log(`${name} connected..`);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message=>{
+        console.log(message);
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+    });
+
+    socket.on('disconnect', data=>{
+        console.log(`${users[socket.id]} disconnected..`);
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+});
+
+app.use(express.static("public"));
+
+server.listen(8000, ()=>{
+    console.log("Server runnning on http://localhost:8000");
+});
